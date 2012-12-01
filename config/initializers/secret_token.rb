546fea3424f9d522f938abfef837e352377e7763
gxw <gxw@gxw.local>
2012-12-01 08:52:47 +0800

    项目搭建

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restaurant::Application.config.secret_token = 'bdcb44d886091edc17fe71c1aa16b8b1273d01a24568e924edf1cefc85c9db0bc86abfbec413e80d0bb4c09766f9a30ef63be1e37585331cce3d292517653e45'
