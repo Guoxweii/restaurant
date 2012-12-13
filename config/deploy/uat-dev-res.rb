7492bb6e8d7529a9073cb690f91aa6ccc5883eda
gxw <gxw@gxw-air.lan>
2012-12-13 10:02:03 +0800

    解决发布脚本的问题

--- config/deploy/uat-dev-res.rb
+++ config/deploy/uat-dev-res.rb
@@ -3,5 +3,5 @@ set :deploy_to, "/var/www/gxw/apps/#{application}"
 set :user, "www-data"
 server "58.196.13.10", :app, :web, :db, :primary => true, :whenever => true
 
-set :asset_env, "RAILS_RELATIVE_URL_ROOT=/#{application}"
+# set :asset_env, "RAILS_RELATIVE_URL_ROOT=/#{application}"
 default_environment["http_proxy"] = default_environment["https_proxy"] = "http://58.196.13.38:8123"
