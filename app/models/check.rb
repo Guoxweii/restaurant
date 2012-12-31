0b07ca41ee9147cbbd3fff71a6ed8749b4c5b4e6
gxw <gxw@gxw-air.lan>
2012-12-31 16:42:23 +0800

    样式修改

--- app/models/check.rb
+++
@@ -1,4 +0,0 @@
-class Order < ActiveRecord::Base
-  attr_accessible :ip_address, :name, :address, :telephone, :email, :ok_time
-  has_many :varieties
-end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,5 @@
 class Order < ActiveRecord::Base
   attr_accessible :num, :varieties, :ip_address, :name, :address, :telephone, :email, :ok_time
   has_and_belongs_to_many :varieties
-  belongs_to :check
   validates_presence_of :num, :address, :telephone
 end
