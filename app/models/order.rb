527f12b6e1d2e846824fa9fc086ba2cc83dd563a
gxw <alphaguoxiongwei@gmail.com>
2013-01-12 23:22:47 +0800

    增加定时任务

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,9 @@ class Order < ActiveRecord::Base
   has_many :checks
   validates_presence_of :address, :telephone
   symbolize :state, :in => [:online, :offline], :scopes => true
+  
+  def self.delete_unuseful_data
+    Order.offline.where("created_at < ?", Date.yesterday).destroy_all
+    Check.where("order_id is NULL AND created_at < ?", Date.yesterday).destroy_all
+  end
 end
